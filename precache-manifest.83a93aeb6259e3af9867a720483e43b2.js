5a7a8a7f4abdb0029f6c26edc36ac1df79ea01ed
Khushi2105 <khushisharma.email@gmail.com>
2020-08-25 16:51:48 +0530

    Updates

---
+++ precache-manifest.83a93aeb6259e3af9867a720483e43b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99eca16297e1af5a281cd0c70dc08d02",
+    "url": "/monstersrolodex/index.html"
+  },
+  {
+    "revision": "68073cb1c3a537f256a2",
+    "url": "/monstersrolodex/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/monstersrolodex/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersrolodex/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68073cb1c3a537f256a2",
+    "url": "/monstersrolodex/static/js/main.055d72ce.chunk.js"
+  },
+  {
+    "revision": "6a9450ea30fb3ef95394",
+    "url": "/monstersrolodex/static/js/runtime-main.3597021f.js"
+  }
+]);
